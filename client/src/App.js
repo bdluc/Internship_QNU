5a490faf5d208cdee9dd609a66819f5cbafcbab6
hoadang <hoadang24497@gmail.com>
2019-04-04 14:38:23 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,59 @@ import React, { Component } from 'react';
 import Routes from '../src/components/Routes';
 import TopNavigation from './components/topNavigation';
 import SideNavigation from './components/sideNavigation';
+import Login from './components/pages/LoginPage'
 import Footer from './components/Footer';
 import './index.css';
 
 class App extends Component {
-  
-  render() {
-    return (
+  constructor(props){
+    super(props);
+    this.state = {
+      isLogin : false,
+      dataLogin : "",
+    };
+  }
+
+  onLogin (value) {
+    if (value !== null) {
+      this.setState({
+        isLogin : value
+      });
+    }    
+  }
+
+  changeComponent () {
+    console.log(localStorage.getItem('user') )
+    if (localStorage.getItem('user') === null) {
+      return (
+      <div className="flexible-content">
+      <main id="content" className="p-5">
+      <Login onLogin = {this.onLogin.bind(this)}></Login>
+      </main>
+      </div>
+      )
+    } else {
+      return (
         <div className="flexible-content">
-          <TopNavigation />
+          <TopNavigation onLogin = {this.onLogin.bind(this)}/>
           <SideNavigation />
+          <div>
           <main id="content" className="p-5">
-            <Routes />
+            <Routes></Routes>
           </main>
           <Footer />
+          </div>        
         </div>
+      )
+    }
+  }
+  
+  render() {
+    let component = this.changeComponent();
+    return (
+      <div>
+          {component}
+      </div>
     );
   }
 }
