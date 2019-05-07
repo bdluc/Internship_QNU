a4a95c4b09ac958b101f18071544248de1dff3ce
BuiChiBach <buichibach97@gmail.com>
2019-05-07 13:46:22 +0700

    update HomePage for intern

---
+++ client/src/components/pages/sections/HomePageForIntern.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReportPage from './components/ReportPage';
+class HomePageForIntern extends React.Component {
+
+  render() {
+    return (
+
+      <div>
+        <ReportPage></ReportPage>
+      </div>
+
+    );
+  }
+}
+
+export default HomePageForIntern
